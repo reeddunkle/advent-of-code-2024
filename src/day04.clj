4069f7fbbd874b49f7479e50f0f5b4f524cf3b8c
Reed Dunkle <11543198+reeddunkle@users.noreply.github.com>
2024-12-05 12:16:53 -0500

    day04

--- src/day04.clj
+++ src/day04.clj
@@ -7,8 +7,18 @@
 (defn get-coords [lines coords]
   (map (fn [[y x]] (get-in lines [y x])) coords))
 
-(defn is-match [chars]
+(defn calculate-coords [y x steps]
+  (map (fn [step]
+         [(+ y (first step)) (+ x (second step))])
+       steps))
+
+(defn is-match-1 [chars]
   (some #(= "XMAS" %)
+        [(str/join "" chars)
+         (str/join "" (reverse chars))]))
+
+(defn is-match-2 [chars]
+  (some #(= "MAS" %)
         [(str/join "" chars)
          (str/join "" (reverse chars))]))
 
@@ -22,16 +32,17 @@
                [[1 1] [2 2] [3 3]]] ;; Diagonal right
         ]
     (reduce
-     (fn [total y1]
+     (fn [total y]
        (reduce
-        (fn [count x1]
+        (fn [count x]
           (reduce
            (fn [inner-count step]
-             (let [coords (map #(vector (+ y1 (first %)) (+ x1 (second %))) step)
+             (let [calculated-coords (calculate-coords y x step)
+                   coords (cons [y x] calculated-coords)
                    [last-y last-x] (last coords)
                    valid? (within-bounds? last-y last-x max-y max-x)]
                (if
-                (and valid? (is-match (get-coords lines (cons [y1 x1] coords))))
+                (and valid? (is-match-1 (get-coords lines coords)))
                  (inc inner-count)
                  inner-count)))
            count
@@ -41,5 +52,37 @@
      0
      (range max-y))))
 
-(solve1 (slurp "src/inputs/day04"))
+(defn solve2 [data]
+  (let [lines (mapv vec (str/split-lines data))
+        max-y (count lines)
+        max-x (count (first lines))
+        steps [;; Down-right
+               [[0 0]
+                [1 1]
+                [2 2]]
+               ;; Down-left
+               [[0 2]
+                [1 1]
+                [2 0]]]]
+
+    (reduce
+     (fn [total y]
+       (reduce
+        (fn [count x]
+          (let [coordsA (calculate-coords y x (first steps))
+                coordsB (calculate-coords y x (second steps))
+                [[last-yA last-xA] [last-yB last-xB]] [(last coordsA) (last coordsB)]
+                valid? (and (within-bounds? last-yA last-xA max-y max-x)
+                            (within-bounds? last-yB last-xB max-y max-x))]
+            (if (and valid?
+                     (is-match-2 (get-coords lines coordsA))
+                     (is-match-2 (get-coords lines coordsB)))
+              (inc count)
+              count)))
+        total
+        (range max-x)))
+     0
+     (range max-y))))
+
+(solve2 (slurp "src/inputs/day04"))
 
